f5025de753dcbca66d69a99b51d3ead7e8974d95
IFives <47913528+IFives@users.noreply.github.com>
2020-02-29 15:23:09 +0100

    Create likehome.sh

---
+++ likehome.sh
@@ -0,0 +1,38 @@
+!/bin/bash
+
+echo "Welcome to your new Debian/Ubuntu based OS !"
+
+apt update
+apt -y upgrade
+
+echo "Are you on a High Density of Pixel Display (More than 1080p) ? yes or no"
+read answerSpotify
+echo "${answerSpotify}"
+
+#echo "Do you use GNOME ? yes or no"
+#read answerGnome
+#echo "${answerGnome}"
+
+#Install and configure VIM
+apt -y install vim
+cp .vimrc /home/${USER}/
+
+#Install and configure Spotify if you have a HiDPi
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+apt -y install spotify-client
+if [ ${answerSpotify} = "yes" ] 
+then
+        sudo cp spotify.desktop /usr/share/applications/
+fi
+
+apt -y install gnome-tweak-tool
+
+add-apt-repository ppa:papirus/papirus
+apt -y install papirus-icon-theme
+
+apt -y install thunderbird
+
+apt -y install snapd
+
+snap install discord
