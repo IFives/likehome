7eeef94f9298ee436a758c93ed8ccfcc64e94a2b
IFives <f.pirckher@gmail.com>
2020-03-07 22:17:34 +0100

    Adding logs in /var/log/likehome.log and some minor changes

--- likehome.sh
+++ likehome.sh
@@ -1,18 +1,22 @@
 !/bin/bash
 
+exec 1 && exec 2 > /var/log/likehome.log
+
 echo "Welcome to your new Debian/Ubuntu based OS !"
 
 apt update
 apt -y upgrade
 
+#Future Integration for HiDPI
 #echo "Are you on a High Density of Pixel Display (More than 1080p) ? yes or no"
 #read answerSpotify
 
+#Future Integration to choose between Gnome and KDE
 #echo "Do you use GNOME ? yes or no"
 #read answerGnome
 #echo "${answerGnome}"
 
-echo "What's your home folder name ? (Ex: /home/username)"
+echo "What's your home folder's name ? (Ex: /home/username)"
 read usrName
 
 #Install and configure VIM
@@ -30,8 +34,14 @@ apt -y install spotify-client
         sudo cp spotify.desktop /usr/share/applications/
 #fi
 
-apt -y install gnome-tweak-tool
+sudo apt -y install chromium
 
+#Install Gnome themes and extensions
+apt -y install gnome-tweak-tool
+apt -y install gnome-shell-extensions
+apt -y install chrome-gnome-shell
+
+#Install the papirus icons pack
 add-apt-repository ppa:papirus/papirus
 apt -y install papirus-icon-theme
 
@@ -43,6 +53,9 @@ sleep 1000
 
 snap install discord
 
-clear
+apt -y autoremove
 
-echo "The personalisation is finnished, go see the result by yourself !"
+echo "Oh Oh... There are some errors :"
+cat /var/log/likehome.log | grep err
+
+echo "The personalisation is finished, go see the result by yourself !"
