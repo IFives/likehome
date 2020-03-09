3e3a6ed36c6ce53b76df2ec1f3f4eda2084b138c
IFives <f.pirckher@gmail.com>
2020-03-09 15:08:27 +0100

    Add of comment within the log file and colors, plus fixing bugs

--- likehome.sh
+++ likehome.sh
@@ -2,27 +2,39 @@
 #######################################################################
 #Author : IFives
 #Date of creation : 01/03/2020
-#Last modification date : 08/03/2020
+#Last modification date : 09/03/2020
 #######################################################################
 
 #Put everything done by the script (tee command) and at the same time put it in the terminal, so it can be read by the user
 exec 2>&1 | tee /var/log/likehome.log
+
+#Variables to color the text
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+
+#No color
+NC='\033[0m'
 
 dateHour=$(date +%d-%m-%Y %H:%M)
 
 #Put date and hour at the beginning of the log file
 echo ${dateHour}
 
-echo "Welcome to your new Debian/Ubuntu based OS !"
+echo "${GREEN}Welcome to your new Debian/Ubuntu based OS !${NC}"
 
-#Add repo to install papirus icons pack
+echo -e "${CYAN}Adding repo to install papirus icons pack ${NC}"
 add-apt-repository ppa:papirus/papirus
 
-#Add repo to install spotify
+echo -e "${CYAN}Adding repo to install spotify${NC}"
 curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
 
-#Update the package from the repos and uprade packages of the computer
+echo -e "${CYAN}Updating the list of repos and upgrading packages of the computer ${NC}"
 apt update
 apt -y upgrade
 
@@ -35,19 +47,19 @@ apt -y upgrade
 #read answerGnome
 #echo "${answerGnome}"
 
-echo "What's your home folder's name ? (Ex: /home/username; Username in lower case)"
+echo "${GREEN} What's your home folder's name ? (Ex: /home/username; Username in lower case) ${NC}"
 read usrName
 
-#Install Graphical for Uncomplicated Firewall
+echo -e "${CYAN} Installing Graphical for Uncomplicated Firewall ${NC}"
 apt -y install gufw
 
-#Install and configure VIM
+echo -e "${CYAN} Installing and configuring VIM, to install plugins refer to junegunn/vim-plug in Github ${NC}"
 apt -y install vim
 cp .vimrc /home/${usrName}/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#Install and configure Spotify if you have a HiDPi
+echo -e "${CYAN} Installing and configuring Spotify if you have a HiDPi ${NC}"
 apt -y install spotify-client
 #if [ ${answerSpotify} = "yes" ] 
 #then
@@ -56,34 +68,34 @@ apt -y install spotify-client
 
 sudo apt -y install chromium
 
-#Install Gnome themes and extensions
+echo -e "${CYAN} Installing Gnome themes and extensions ${NC}"
 apt -y install gnome-tweak-tool
 apt -y install gnome-shell-extensions
 apt -y install chrome-gnome-shell
 
-#Install the papirus icons pack
+echo -e "${CYAN} Installing the papirus icons pack ${NC}"
 apt -y install papirus-icon-theme
 
-#Install the mail client thunderbird
+echo -e "${CYAN} Installing the mail client thunderbird ${CN}"
 apt -y install thunderbird
 
-#Install snapcraft package manager
+echo -e "${CYAN} Installing snapcraft package manager ${CN}"
 apt -y install snapd
 
 #Wait 1 second, there can be some errors if you install snap packages directly after installing another package
-sleep 1000
+sleep 1
 
-#Install Discordvia snapcraft
+echo -e "${CYAN} Installing Discord via snapcraft ${CN}"
 snap install discord
 
-#Install molotov TV
+echo -e "${CYAN} Installing molotov TV ${CN}"
 wget 'https://www.molotov.tv/download' -O molotov.Appimage
 chmod +x molotov.Appimage
 
-#Check if there some old packages that needs to be revomed
+echo -e "${CYAN} Check if there some old packages that needs to be revomed ${CN}"
 apt -y autoremove
 
-#Checking if there are errors
+echo -e "${CYAN} Checking if there are errors ${CN}"
 testerr=cat /var/log/likehome.log | grep err
 if [ ! -z ${testerr}  ]
 then
